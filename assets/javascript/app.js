06b9a556af34fab9797a7807165446c3f4fd1afb
Connor F <connorfinnigan@gmail.com>
2019-03-12 00:43:33 -0500

    styling, timers, hide/show, html updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,79 @@
 $(document).ready(function() {
 
+    var time = 30;
+    var intervalId;
+    var wins = 0;
+    var losses = 0;
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
+
+    function hidePage2() {
+        $("#page2").hide();
+    }
+
+    hidePage2();
+
+    function showPage2() {
+        $("#page2").show();
+    }
+
+    function hidePage1() {
+        $("#page1").hide();
+    }
+
+    function showPage1() {
+        $("#page1").show();
+    }
+
+    // $("#title2").hide();
+    // $("#correct-answers").hide(); 
+    // $("#incorrect-answers").hide();
+    // $("#unanswered").hide();
+    // $(".button2").hide();
+
+    // function that starts the clock
+    function run() {
+        intervalId = setInterval(decrement, 1000);
+    }  
+
+    run();
+
+    function stop() {
+        clearInterval(intervalId);
+      }
+
+    // function that counts down the seconds
+    function decrement() {
+        time--;
+        $(".time-remaining").html("<h2>Time Remaining: " + time + " seconds </h2>");
+        if (time === 0) {
+            stop();
+            alert("Time's Up!")
+        }
+    }
+
+    // function that sets time back to 30 seconds
+    function resetTime() {
+        time = 30000;
+    }
+
         //  Start on click.
-        $("#button1").on("click", function() {
-            //  Set the button alert's timeout to run three seconds after the function's called.
-            testAlert = setTimeout(function() {
-            alert("Test Alert.");
-            }, 1000);
+        $("#submit").on("click", function() {
+            hidePage1();
+            showPage2();
+            // clear page contents
+            // check if all the questions are correct
+            // increase wins or losses, and display those
+            // display counts for correct, incorrect, and unanswered
+        });
+
+        //  Start on click.
+        $("#play-again").on("click", function() {
+            // reload 
+            // hide all previous elements
+            // increase wins or losses, and display those
+            // display counts for correct, incorrect, and unanswered
         });
 
 });
